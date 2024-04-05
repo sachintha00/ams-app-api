3e3347ce7c5dacf49c28c8beb57c631cda858454
sachintha00 <sachintha0603@gmail.com>
2024-04-05 13:04:15 +0530

    #1 Feat: create user login, role and permission and add sub users

---
+++ src/app/Http/Controllers/ActivityLogController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use  Spatie\Activitylog\Models\Activity;
+
+class ActivityLogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $Activity = Activity::all();
+
+        //return json response
+        return response()->json([
+            "status" => true,
+            'Permission' => $Activity
+        ],200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ src/routes/api.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\PermisionController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ActivityLogController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix('v1')->group(function(){
+
+    Route::post("register", [AuthController::class, "register"]);
+    Route::post("login", [AuthController::class, "login"]);
+    Route::post('account/verify', [AuthController::class, 'verifyAccount'])->name('user.verify'); 
+
+    Route::group([
+        "middleware" => ["auth:api","is_verify_email"]
+    ], function(){
+        Route::get("profile", [AuthController::class, "profile"]);
+        Route::get("logout", [AuthController::class, "logout"]);
+
+        Route::get("users", [UserController::class, "index"]);
+        Route::post("addusers", [UserController::class, "store"]);
+
+        Route::get("allpermissions", [PermisionController::class, "index"]);
+        Route::post("addpermissions", [PermisionController::class, "store"]); 
+        Route::put('permissionsupdate/{id}', [PermisionController::class, 'update']);
+        Route::delete('permissionsdelete/{id}', [PermisionController::class, 'destroy']);
+
+        Route::get("allroles", [RoleController::class, "index"]);
+        Route::post("addroles", [RoleController::class, "store"]);
+        Route::put('rolesupdate/{id}', [RoleController::class, 'update']);
+        Route::delete('rolesdelete/{id}', [RoleController::class, 'destroy']);
+        Route::get("roles/{id}/give-permissions", [RoleController::class, "addPermissionToRole"]);
+        Route::put("roles/{id}/give-permissions", [RoleController::class, "givePermissionToRole"]);
+
+        Route::get("All-Activitys", [ActivityLogController::class, "index"]);
+    });
+
+
+});
+
